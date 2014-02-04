9b8ada3063044a736ca49452b4250e1847da9f45
Blin Sébastien <AmarOk@localhost.localdomain>
2014-02-04 12:27:43 +0100

    Ajouts de sources

--- Reconnaissance.py
+++ Reconnaissance.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 
 #   ooooooooooo ooooo oooooooooo ooooooooooo      o      oooooooooo   oooooooo8  
-#   88  888  88  888   888    888 888    88      888      888    888 888         
+#   88  888  88  888   888    888 888            888      888    888 888         
 #       888      888   888oooo88  888ooo8       8  88     888oooo88   888oooooo  
-#       888      888   888        888    oo    8oooo88    888                888 
+#       888      888   888        888          8oooo88    888                888 
 #      o888o    o888o o888o      o888ooo8888 o88o  o888o o888o       o88oooo888
 
 #test n0m1s
