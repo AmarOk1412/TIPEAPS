87fad12c212b51789e35922fcfa22581f1fa4818
n0m1s <nomis@macadms-MacBook-Pro.local>
2014-02-04 11:29:20 +0100

    test

--- Reconnaissance.py
+++ Reconnaissance.py
@@ -4,8 +4,10 @@
 #   88  888  88  888   888    888 888    88      888      888    888 888         
 #       888      888   888oooo88  888ooo8       8  88     888oooo88   888oooooo  
 #       888      888   888        888    oo    8oooo88    888                888 
-#      o888o    o888o o888o      o888ooo8888 o88o  o888o o888o       o88oooo888  
-                                                                             
+#      o888o    o888o o888o      o888ooo8888 o88o  o888o o888o       o88oooo888
+
+#test n0m1s
+
 #Imports
 import cv2
 import numpy
