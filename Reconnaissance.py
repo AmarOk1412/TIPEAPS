7d73ffb1bbb97f878c9db73ca60dcf0d9c6846c1
Blin Sébastien <AmarOk@localhost.localdomain>
2014-02-04 11:26:51 +0100

    Récupération de la webcam

--- Reconnaissance.py
+++ Reconnaissance.py
@@ -9,12 +9,42 @@ import sys
 
 #Constantes
 TRAINSET = "lbpcascade_frontalface.xml"    #Fichier de reconnaissance
-DOWNSCALE = 4                              
 IMAGE_SIZE = 170                           #Normalisation des images de base
-NUMBER_OF_CAPTURE = 10                     #Nombre de captures à réaliser pour la base de données
+NUMBER_OF_CAPTURE = 10                     #Nombre de captures a realiser pour la base de donnees
 SEUIL = 180                                #Seuil de reconnaissance
+CAMERA = 0                                 #La camera
 
 #####################################
+class CreateDataBase():
+    def __init__(self):
+        self.rval = False            
+        self.camera = cv2.VideoCapture(CAMERA)
+
+    def capture(self):        
+        if self.camera.isOpened():
+            rval, frame = self.camera.read()
+        else:
+            rval = False
+
+        while rval:
+            rval, frame = self.camera.read()
+            cv2.imshow("Create Database Window", frame)
+            key = cv2.waitKey(20)
+            if key in [27, ord('Q'), ord('q')]:
+                break
+        
+
+#class RecognizeEigenFaces():
+#    def __init__(self):
+#        self.camera = CAMERA
+
+#class RecognizeLPBH():
+#    def __init__(self):
+#        self.camera = CAMERA
+
+if __name__ == "__main__":
+    createDB = CreateDataBase()
+    createDB.capture()	
 
 
 
