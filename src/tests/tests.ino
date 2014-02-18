4a874b7c76dafe2a6df0f10107833d11a6f15554
TheMrNomis <n0m1s@homnomnom.fr>
2014-02-18 13:14:13 +0100

    arduino

--- src/tests/tests.ino
+++ src/tests/tests.ino
@@ -2,9 +2,32 @@ int ledDemarrage = 7;
 int ledWarning = 6;
 int ledLimiteAcceleration = 5;
 int ledFrein = 4;
+int speaker = 8;
 
 
 void setup()
+{
+  pinMode(ledDemarrage, OUTPUT);
+  pinMode(ledWarning,OUTPUT);
+  pinMode(ledLimiteAcceleration,OUTPUT);
+  pinMode(ledFrein,OUTPUT);
+  
+  digitalWrite(ledDemarrage,HIGH);
+  digitalWrite(ledWarning,HIGH);
+  digitalWrite(ledLimiteAcceleration,HIGH);
+  digitalWrite(ledFrein,HIGH);
+  
+  delay(2000);
+  
+  digitalWrite(ledDemarrage,LOW);
+  digitalWrite(ledWarning,LOW);
+  digitalWrite(ledLimiteAcceleration,LOW);
+  digitalWrite(ledFrein,LOW);
+  
+  tone(speaker,,)
+}
+
+void loop()
 {
   
 }
