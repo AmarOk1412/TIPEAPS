da8bccd396a371c95e706577602ef6d2aec94438
TheMrNomis <n0m1s@homnomnom.fr>
2014-02-23 15:50:59 +0100

    arduino code

--- src/tests/tests.ino
+++ src/tests/tests.ino
@@ -1,7 +1,9 @@
 /**
- *
- *
- *
+ * ooooooooooo  ooooo  oooooooooo ooooooooooo     o      oooooooooo  oooooooo8
+ *  88 888  88   888   888    888 888            888      888   888 888
+ *     888       888   888oooo88  888ooo8       8  88     888oooo88  888oooooo
+ *     888       888   888        888          8oooo88    888               888
+ *    o888o     o888o o888o      o888ooo8888 o88o  o888o o888o      o88oooo888
  */
 
 int ledDemarrage = 7;
